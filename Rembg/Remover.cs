b65d8085161d620051db3fb3f9ea16242f40add4
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-19 22:08:09 +0100

    Add pythonnet dependency, remover class and background remover method

--- Rembg/Remover.cs
+++ Rembg/Remover.cs
@@ -1,7 +1,30 @@
-﻿namespace Rembg
+﻿using Python.Runtime;
+
+
+namespace Rembg
 {
     public class Remover
     {
+        public byte[] RemoveBackground(byte[] imageBytes)
+        {
+            PythonEngine.Initialize();
+            using (Py.GIL())
+            {
+                dynamic rembg = Py.Import("rembg");
+                dynamic image = Py.Import("PIL.Image");
+                using (var inputStream = new MemoryStream(imageBytes))
+                using (var outputStream = new MemoryStream())
+                {
+                    dynamic inputImage = image.open(inputStream);
+                    dynamic outputImage = rembg.remove(inputImage);
+                    outputImage.save(outputStream, "PNG");
+                    return outputStream.ToArray();
+                }
+            }
+        }
+
 
     }
+
+    
 }
