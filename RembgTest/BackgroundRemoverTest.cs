57b2b96b2ef619d9dc6d487681e7c8074fbaa34b
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-20 21:30:57 +0100

    Add rembg test with unit test for remover

---
+++ RembgTest/BackgroundRemoverTest.cs
@@ -0,0 +1,31 @@
+using Rembg;
+using Xunit;
+
+namespace RembgTest
+{
+    public class BackgroundRemoverTest
+    {
+        public BackgroundRemoverTest()
+        {
+            // Set the PYTHONNET_PYDLL environment variable to point to the Python DLL
+
+            var pythonHome = @"C:\Users\Kevin\AppData\Local\Programs\Python\Python38";
+            var pythonDll = Path.Combine(pythonHome, "python38.dll");
+            Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", pythonDll);
+        }
+
+        [Fact]
+        public void TestToRemoveImageBackground()
+        {
+            var remover = new Remover();
+
+            using (var imageStream = File.OpenRead(@"C:\Users\Kevin\Desktop\gym-review-1.png"))
+            {
+                Stream resultStream = remover.RemoveBackground(imageStream);
+
+                Assert.NotNull(resultStream);
+
+            }
+        }
+    }
+}
