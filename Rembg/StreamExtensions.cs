8f576cbfd0e950bdf8348266fb0edff1af22f785
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-20 21:15:22 +0100

    Add stream extensions

---
+++ Rembg/StreamExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Rembg
+{
+    public static class StreamExtensions
+    {
+        public static byte[] ReadAllBytes(this Stream input)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
