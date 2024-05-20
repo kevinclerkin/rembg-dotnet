e9843ff452d68f7b3341b6960b3a1438e5d5ecb5
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-20 21:12:43 +0100

    Create separate pystream abstract class

---
+++ Rembg/PyStream.cs
@@ -0,0 +1,44 @@
+﻿using Python.Runtime;
+
+
+namespace Rembg
+{
+    public class PyStream : Stream
+    {
+        private readonly PyObject _pyStream;
+        private readonly PyObject _readMethod;
+        private int _position;
+
+        public PyStream(PyObject pyStream)
+        {
+            _pyStream = pyStream;
+            _readMethod = _pyStream.GetAttr("read");
+            _position = 0;
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            PyObject result = _readMethod.Invoke(new PyInt(count));
+            byte[] bytes = result.As<byte[]>();
+            Array.Copy(bytes, 0, buffer, offset, bytes.Length);
+            _position += bytes.Length;
+            return bytes.Length;
+        }
+
+        public override bool CanRead => true;
+        public override bool CanSeek => false;
+        public override bool CanWrite => false;
+        public override long Length => throw new NotSupportedException();
+        public override long Position
+        {
+            get => _position;
+            set => throw new NotSupportedException();
+        }
+
+        public override void Flush() => throw new NotSupportedException();
+        public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
+        public override void SetLength(long value) => throw new NotSupportedException();
+        public override void Write(byte[] buffer, int offset, int count) => throw new NotSupportedException();
+
+    }
+}
--- Rembg/Remover.cs
+++ Rembg/Remover.cs
@@ -44,43 +44,4 @@ namespace Rembg
     }
 
 
-    public class PyStream : Stream
-    {
-        public override bool CanRead => throw new NotImplementedException();
-
-        public override bool CanSeek => throw new NotImplementedException();
-
-        public override bool CanWrite => throw new NotImplementedException();
-
-        public override long Length => throw new NotImplementedException();
-
-        public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public override void Flush()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override int Read(byte[] buffer, int offset, int count)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override long Seek(long offset, SeekOrigin origin)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void SetLength(long value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Write(byte[] buffer, int offset, int count)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-
 }
