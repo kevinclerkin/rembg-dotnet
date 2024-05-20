a35daca2e0b600bfdfc7c5117a8f102616507b7d
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-20 21:35:22 +0100

    Add console app

---
+++ RembgApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Rembg;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Define input and output image paths
+        string inputImagePath = @"C:\Users\Kevin\Desktop\gym-review-1.png";
+        string outputImagePath = @"C:\Users\Kevin\Desktop\gym-review-2.png";
+
+        // Read input image as a stream
+        using (var inputImageStream = File.OpenRead(inputImagePath))
+        {
+            // Create an instance of the Remover class
+            var remover = new Remover();
+
+            // Remove background and get the resulting stream
+            using (var resultStream = remover.RemoveBackground(inputImageStream))
+            {
+                // Save the resulting image stream to an output file
+                using (var outputFileStream = File.Create(outputImagePath))
+                {
+                    resultStream.CopyTo(outputFileStream);
+                }
+            }
+        }
+
+        Console.WriteLine("Background removal completed. Result saved to: " + outputImagePath);
+    }
+}
