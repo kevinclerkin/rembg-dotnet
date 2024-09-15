9d3f647a68fdaccd3514adea5a8c3113051b59d6
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-09-15 23:42:33 +0100

    Add pystream test for null pystream

---
+++ RembgTest/PyStreamTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FakeItEasy;
+using Rembg;
+using Python.Runtime;
+
+namespace RembgTest
+{
+    public class PyStreamTests
+    {
+        [Fact]
+        public void Constructor_ThrowsArgumentNullException_WhenPyStreamIsNull()
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new PyStream(null!));
+        }
+
+        
+
+
+    }
+}
